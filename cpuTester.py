29a4e939a410719e9821bdb33bc8eb1a30aeb9e7
Yusuf Ahmed <yusuf65820@gmail.com>
2025-12-22 19:19:16 +0000

    test file

---
+++ cpuTester.py
@@ -0,0 +1,11 @@
+import random
+from liarsDice import cpuBet
+def testBetting():
+    allPlayerHands = [[],[]]
+    for carrier in range(2):
+        for carrier2 in range(5):
+            allPlayerHands[carrier].append(random.randint(1,6))
+    diceFace, minCount = cpuBet(allPlayerHands, 1, 0, 3, 0, 0, 1, 3, 3)
+    assert (diceFace >= 0)
+    assert (minCount < 0)
+testBetting()
