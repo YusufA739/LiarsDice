78796862230253f83dd0da17479da3248d87aa37
Yusuf Ahmed <yusuf65820@gmail.com>
2025-12-30 00:04:47 +0000

    cd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="liarsDice",
+      version="0.5",
+      packages=["liarsDice"],
+      install_requires=[],
+      author="Yusuf739",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
